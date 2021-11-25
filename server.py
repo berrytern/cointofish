48007b4f0d1bffea0bcf3f361da732bf6794176d
unknown <berrytern@gmail.com>
2021-11-25 13:47:35 -0300

    fix: sleep time.

--- server.py
+++ server.py
@@ -10,13 +10,14 @@ max_interval_btw_clicks=1300#1.3s
 while True:
     for box in list(pyautogui.locateAllOnScreen("./src/static/fish_config.png")):
         pyautogui.click(box[0]+4,box[1]+4,duration=randint(30,max_interval_btw_clicks)/1000)
+        pyautogui.sleep(2)
         dropdown=pyautogui.locateOnScreen("./src/static/fish_drop_down.png")
-        pyautogui.sleep(0.2)
         if dropdown: 
             feed_button=pyautogui.locateOnScreen("./src/static/fish_feed.png")
-            pyautogui.click(dropdown[0],dropdown[1],duration=randint(30,max_interval_btw_clicks)/1000)
-            pyautogui.click(dropdown[0],dropdown[1]+42,duration=randint(30,max_interval_btw_clicks)/1000)
-            pyautogui.click(feed_button[0]+4,feed_button[1]+4,duration=randint(30,max_interval_btw_clicks)/1000)
+            if feed_button:
+                pyautogui.click(dropdown[0],dropdown[1],duration=randint(30,max_interval_btw_clicks)/1000)
+                pyautogui.click(dropdown[0],dropdown[1]+42,duration=randint(30,max_interval_btw_clicks)/1000)
+                pyautogui.click(feed_button[0]+4,feed_button[1]+4,duration=randint(30,max_interval_btw_clicks)/1000)
         close=pyautogui.locateOnScreen("./src/static/fish_close.png")
         pyautogui.click(close[0]+4,close[1]+4,duration=randint(30,max_interval_btw_clicks)/1000)
     pyautogui.sleep(interval)
